1bed7250ae98119d7c6fd5810ad273b8c82500c4
Rahul <vaish.rahul4810@gmail.com>
2019-10-14 12:49:28 +0530

    JAVA8

---
+++ Java8/src/lambda/Example1Java8.java
@@ -0,0 +1,94 @@
+package lambda;
+
+// If you compare Java7 way with this Java8 way, you will find that:
+// [1] In Java8 interface (meant for Lambda) will have only one method.To ensure this we use @FunctionalInterface
+// [2] Writing Lambda expression is evolution to Anonymous Class style ! 
+
+
+@FunctionalInterface
+interface Calculator1 {
+	void greetCalculator();
+}
+
+@FunctionalInterface
+interface Calculator2 {
+	int sum(int a, int b);
+}
+
+//==============================================
+interface Calculator3 {
+	default int sum(int a, int b) {
+		return a+b;
+	}
+	static int mul(int a, int b) {
+		return a*b;
+	}
+}
+
+class Calculator3Impl implements Calculator3{
+	
+}
+//==============================================
+
+public class Example1Java8 {
+
+	public static void main(String[] args) {
+
+		//EVOLUTION#1
+		/*Calculator1 cal1 = new Calculator1() {
+
+			@Override
+			public void greetCalculator() {
+				System.out.println("Hi Calculator");
+			}
+		};*/
+		
+		//EVOLUTION#2
+		/*Calculator1 cal1 = () {
+		@Override
+		public void greetCalculator() {
+			System.out.println("Hi Calculator");
+		}
+	    };*/
+		
+		//EVOLUTION#3
+		/*Calculator1 cal1 = () {
+			System.out.println("Hi Calculator");
+	    };*/
+		
+		//EVOLUTION#4
+		/*Calculator1 cal1 = () -> {
+				System.out.println("Hi Calculator");
+		};*/
+		
+		//EVOLUTION#5
+		Calculator1 cal1 = () -> System.out.println("Hi Calculator");
+		//NOTE[ABOVE]:
+		// () means no parameters. 
+		// After -> The statement is returning nothing, therefore void.
+		// Interface Calculator1 has a method that satisfies this requirement, therefore all is well !
+		Calculator2 cal2 = (a,b) -> {
+			if(a<0 || b<0) {
+				return 0;
+			}
+			return a+b;
+		};
+		//NOTE[ABOVE]:
+		// (a,b) means two parameters are expected. 
+		// After -> The statement is returning an integer value.
+		// Interface Calculator2 has a method that satisfies this requirement, therefore all is well !
+		
+		cal1.greetCalculator();
+		System.out.println(cal2.sum(3, 3));
+		//==============================================
+		Calculator3 cal3 = new Calculator3Impl();
+		System.out.println(cal3.sum(3, 4));
+		System.out.println(Calculator3.mul(2, 4));
+		//==============================================
+	}
+}
+
+
+
+
+
---
+++ Java8/src/lambda/Example2Java7.java
@@ -0,0 +1,65 @@
+package lambda;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+class Person {
+	String firstName;
+	String secondName;
+	
+	public Person(String firstName, String secondName) {
+		super();
+		this.firstName = firstName;
+		this.secondName = secondName;
+	}	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getSecondName() {
+		return secondName;
+	}
+	public void setSecondName(String secondName) {
+		this.secondName = secondName;
+	}
+	@Override
+	public String toString() {
+		return "Person [firstName=" + firstName + ", secondName=" + secondName + "]";
+	}
+	
+}
+
+class secondNameComp implements Comparator<Person>{
+
+	@Override
+	public int compare(Person o1, Person o2) {
+		return o1.getSecondName().compareTo(o2.getSecondName()); 
+	}
+}
+
+public class Example2Java7 {
+
+	public static void main(String[] args) {
+		List personList = new ArrayList();
+		personList.add(new Person("Rahul","Vaish"));
+		personList.add(new Person("Shah","Alam"));
+		personList.add(new Person("Shah","Jahan"));
+		personList.add(new Person("Akbar","Oman"));
+
+		//Sort the list by last name
+		Collections.sort(personList,new secondNameComp());
+		System.out.print(personList);
+		//List those name whose surname ends with 'J'
+
+		
+		
+		
+		
+	}
+	
+
+}
